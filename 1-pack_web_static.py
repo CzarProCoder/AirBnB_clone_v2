dbd074c1e49c5d11839cd17894697814f2652ab7
CzarProCoder <juliusczar36@gmail.com>
2023-07-06 13:44:14 -0700

    Added a Fabric script that generates a .tgz archive from the contents of the web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''
 Fabric script that generates a .tgz archive from the contents
-	of the web_static folder
+    of the web_static folder
 '''
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     '''
