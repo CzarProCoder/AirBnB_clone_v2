bf545b052d6aa1f5b86888129ed1d98b30016d1d
CzarProCoder <juliusczar36@gmail.com>
2023-07-06 13:42:59 -0700

    Added a Fabric script that generates a .tgz archive from the contents of the web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Fabric script that generates a .tgz archive from the contents
+	of the web_static folder
+'''
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    '''
+    Function to compress files within the directory
+
+    Return:
+        Path to archive upon success
+        else fail
+    '''
+    now = datetime.now()
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + now + '.tgz'
+
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
+
+    # Check if archiving was successful
+    if result.succeeded:
+        return archive_path
+    return None
