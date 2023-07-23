ebd2ce771c2ea0db5bb2a6918864e64794625d9e
CzarProCoder <juliusczar36@gmail.com>
2023-07-23 11:36:35 +0300

    Added a script that starts a Flask web application with 2 routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,13 +11,13 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     '''Display text'''
-    return ('Hello HBNB!')
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello():
-    '''Display text'''
-    return ('HBNB!')
+def hbnb():
+    '''Display text for /hbnb router'''
+    return 'HBNB!'
 
 
 if __name__ == '__main__':
