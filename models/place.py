0b677f9259c0ec923373fa3c135e8d4e57ce7d01
CzarProCoder <juliusczar36@gmail.com>
2023-07-23 17:42:24 +0300

    Added a script to print the state id and state name via html

--- models/place.py
+++ models/place.py
@@ -7,7 +7,7 @@ import models
 from os import getenv
 from models.base_model import Base
 from models.base_model import BaseModel
-from models.amenity import Amenity
+# from models.amenity import Amenity
 from models.review import Review
 from sqlalchemy import Column
 from sqlalchemy import Float
