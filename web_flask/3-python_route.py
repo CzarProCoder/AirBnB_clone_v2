1b00dcfb6b23f3431a88c59a91f0558260a56bc7
CzarProCoder <juliusczar36@gmail.com>
2023-07-23 12:42:15 +0300

    Implemented a flask script for "python" associated routes and sub-directories

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+Script that starts a Flask web application
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Display text'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Display text for /hbnb router'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    '''Function for a variable route'''
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonrt(text='is cool'):
+    '''Function for python route and sub directories'''
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+if __name__ == '__main__':
+    '''Entry Point'''
+    app.run(host='0.0.0.0', port=5000)
